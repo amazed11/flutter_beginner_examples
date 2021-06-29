ee747232eaa4d59ef98e807753e3ecef0f1459b9
amazed11 <amazedking2@gmail.com>
2021-06-29 20:43:42 +0545

    gridview

--- lib/screens/gridview/gridview_screen.dart
+++ lib/screens/gridview/gridview_screen.dart
@@ -24,68 +24,70 @@ class GridViewScreeen extends StatelessWidget {
             ),
             itemCount: 10,
             itemBuilder: (context, index) {
-              return Card(
-                elevation: 10.0,
-                child: GridTile(
-                  header: FlutterLogo(
-                    size: 70.0,
+              return Container(
+                child: Card(
+                  elevation: 10.0,
+                  child: GridTile(
+                    header: FlutterLogo(
+                      size: 70.0,
+                    ),
+                    footer: Text("Rs100"),
+                    child: Text("data"),
+                    // color:
+                    //     Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                    // child: Column(
+                    //   children: [
+
+                    //     // FlutterLogo(
+                    //     //   size: 50.0,
+                    //     // ),
+                    //     // Container(
+                    //     //   width: MediaQuery.of(context).size.width * 0.40,
+                    //     //   child: Text(
+                    //     //     "Flutter Beginners Course Part -1 Flutter Beginners Course Part -1 Flutter Beginners Course Part -1",
+                    //     //     overflow: TextOverflow.ellipsis,
+                    //     //     style: TextStyle(
+                    //     //       color: Colors.white,
+                    //     //       fontSize: 15.0,
+                    //     //       fontWeight: FontWeight.bold,
+                    //     //     ),
+                    //     //   ),
+                    //     // ),
+
+                    //     // Center(
+                    //     //   child: Text(
+                    //     //     "\$100",
+                    //     //     style: TextStyle(
+                    //     //       color: Colors.white,
+                    //     //       fontSize: 18.0,
+                    //     //       fontWeight: FontWeight.bold,
+                    //     //     ),
+                    //     //   ),
+                    //     // ),
+                    //     // Center(
+                    //     //   child: Text(
+                    //     //     "\$150",
+                    //     //     style: TextStyle(
+                    //     //       color: Colors.white,
+                    //     //       fontSize: 14.0,
+                    //     //       fontWeight: FontWeight.bold,
+                    //     //       decoration: TextDecoration.lineThrough,
+                    //     //     ),
+                    //     //   ),
+                    //     // ),
+                    //     // Container(
+                    //     //   color: Colors.black26,
+                    //     //   child: IconButton(
+                    //     //     onPressed: () {
+                    //     //       print("add to cart success");
+                    //     //     },
+                    //     //     color: Colors.white,
+                    //     //     icon: Icon(Icons.add_shopping_cart_outlined),
+                    //     //   ),
+                    //     // ),
+                    //   ],
+                    // ),
                   ),
-                  footer: Text("Rs100"),
-                  child: Text("data"),
-                  // color:
-                  //     Colors.primaries[Random().nextInt(Colors.primaries.length)],
-                  // child: Column(
-                  //   children: [
-
-                  //     // FlutterLogo(
-                  //     //   size: 50.0,
-                  //     // ),
-                  //     // Container(
-                  //     //   width: MediaQuery.of(context).size.width * 0.40,
-                  //     //   child: Text(
-                  //     //     "Flutter Beginners Course Part -1 Flutter Beginners Course Part -1 Flutter Beginners Course Part -1",
-                  //     //     overflow: TextOverflow.ellipsis,
-                  //     //     style: TextStyle(
-                  //     //       color: Colors.white,
-                  //     //       fontSize: 15.0,
-                  //     //       fontWeight: FontWeight.bold,
-                  //     //     ),
-                  //     //   ),
-                  //     // ),
-
-                  //     // Center(
-                  //     //   child: Text(
-                  //     //     "\$100",
-                  //     //     style: TextStyle(
-                  //     //       color: Colors.white,
-                  //     //       fontSize: 18.0,
-                  //     //       fontWeight: FontWeight.bold,
-                  //     //     ),
-                  //     //   ),
-                  //     // ),
-                  //     // Center(
-                  //     //   child: Text(
-                  //     //     "\$150",
-                  //     //     style: TextStyle(
-                  //     //       color: Colors.white,
-                  //     //       fontSize: 14.0,
-                  //     //       fontWeight: FontWeight.bold,
-                  //     //       decoration: TextDecoration.lineThrough,
-                  //     //     ),
-                  //     //   ),
-                  //     // ),
-                  //     // Container(
-                  //     //   color: Colors.black26,
-                  //     //   child: IconButton(
-                  //     //     onPressed: () {
-                  //     //       print("add to cart success");
-                  //     //     },
-                  //     //     color: Colors.white,
-                  //     //     icon: Icon(Icons.add_shopping_cart_outlined),
-                  //     //   ),
-                  //     // ),
-                  //   ],
-                  // ),
                 ),
               );
             }),
